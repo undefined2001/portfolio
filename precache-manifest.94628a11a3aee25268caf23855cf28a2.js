b2443717db98d868fa0a8d0dccbe3681d5496c4f
Undefined <theundefined2001@gmail.com>
2022-12-02 08:19:59 +0600

    Updates

---
+++ precache-manifest.94628a11a3aee25268caf23855cf28a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e815664465b0f1fff23",
+    "url": "/portfolio/static/css/main.22dea615.chunk.css"
+  },
+  {
+    "revision": "6e815664465b0f1fff23",
+    "url": "/portfolio/static/js/main.6e815664.chunk.js"
+  },
+  {
+    "revision": "002a2879119e0b5d31fb",
+    "url": "/portfolio/static/js/1.002a2879.chunk.js"
+  },
+  {
+    "revision": "b1f5f4799482c5c538fb",
+    "url": "/portfolio/static/js/2.b1f5f479.chunk.js"
+  },
+  {
+    "revision": "9967ac179f814adbb1cb",
+    "url": "/portfolio/static/js/runtime~main.9967ac17.js"
+  },
+  {
+    "revision": "fac54844800025595adf9a301bc78039",
+    "url": "/portfolio/static/media/image.fac54844.png"
+  },
+  {
+    "revision": "0b849c72f38362fe12072a4916660013",
+    "url": "/portfolio/static/media/cpp.0b849c72.png"
+  },
+  {
+    "revision": "02711c2cf037c786ebb844af1328f809",
+    "url": "/portfolio/static/media/django.02711c2c.png"
+  },
+  {
+    "revision": "c5e3c2f5e09713ea7bc42a4231401a16",
+    "url": "/portfolio/static/media/python.c5e3c2f5.png"
+  },
+  {
+    "revision": "f9786a11737eb43d582fc9f7224cb73a",
+    "url": "/portfolio/index.html"
+  }
+];
